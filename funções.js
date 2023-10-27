77b7d43080f06e3b82d5a13def4d60fe65a28307
Thata <126881634+Thanaha@users.noreply.github.com>
2023-10-27 08:36:24 -0300

    Add files via upload

---
+++ funções.js
@@ -0,0 +1,26 @@
+// let x = "";
+// console.log(x);
+// x = "oi";
+
+// DECLARAÇÃO DE FUNÇÃO
+
+// 1) declara a função
+                      //2 ->valor de soma()
+function impremeTexto(texto) {
+    console.log(texto)
+}
+
+// 2) executa a função (1 ou + vezes)
+
+impremeTexto(soma());
+// impremeTexto("outro texto");
+
+// três formas de escrever funções
+
+function soma(){
+    //outros códigos
+    //vários console.log()
+    return 2 + 2;
+}
+
+soma()
